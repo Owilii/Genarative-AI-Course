d144a3f4510f08166be42cf25df7aba65b87ffac
Owilii <henryonyango506@gmail.com>
2025-10-02 08:27:30 +0300

    Created an AI chat bot

--- mypyfile.py
+++ mypyfile.py
@@ -5,13 +5,16 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Get API key
-api_key = os.getenv("GEMINI_API_KEY")
+api_key = os.getenv("OPENAI_API_KEY")
+api_key_1 = os.getenv("SERPER_API_KEY")
+
 
 print("API Key loaded:", api_key is not None)  # Just to check it's working
+print("API Key loaded:", api_key_1 is not None)
 
 
 
 import jaclang
 from hello import lovejac
 
-print(f"Python is awosome. But, {lovejac()}")
+print(f"Python is awosome. But, {lovejac()}")# Just to check it's working
